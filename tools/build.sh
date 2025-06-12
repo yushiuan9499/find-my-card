08defc9a59c5fbb2bdeb586ed2d7c251cd7abe39
yushiuan9499 <yushiuan9499@gmail.com>
2025-06-12 15:48:36 +0800

    chore: check dependency

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 檢查Dependencies
+if ! dpkg -s libjsoncpp-dev >/dev/null 2>&1; then
+  echo "Error: libjsoncpp-dev is not installed. Please install it using 'sudo apt-get install libjsoncpp-dev'."
+  exit 1
+fi
+
 # 創建必要的資料夾
 mkdir -p build
 cd build
