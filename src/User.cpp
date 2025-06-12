1507afb7c9807183bc5f3f9a2e443a16a6dd0a58
yushiuan9499 <yushiuan9499@gmail.com>
2025-06-12 14:10:26 +0800

    feat(User): add implement

--- src/User.cpp
+++ src/User.cpp
@@ -1,1 +1,78 @@
 #include "User.h"
+#include "Box.h"
+#include "Card.h"
+#include "EmailServer.h"
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+User::User(const string &usrName, const string &passwd,
+           EmailServer *emailServer, const string &emailAddr,
+           const string &emailPasswd)
+    : username(usrName), password(passwd), email(emailAddr),
+      emailPassword(emailPasswd), emailServer(emailServer) {}
+User::~User() {
+  for (Card *card : cards) {
+    delete card; // Clean up dynamically allocated cards
+  }
+}
+
+void User::addCard(Card *card) {
+  if (card) {
+    cards.insert(card);
+  }
+}
+
+void User::removeCard(Card *card) {
+  if (card) {
+    cards.erase(card);
+  }
+}
+
+bool User::dropCard(Box *box, Card *card) {
+  if (!box || !card) {
+    return false; // Invalid box or card
+  }
+  if (cards.find(card) == cards.end()) {
+    return false; // Card not owned by the user
+  }
+  Card *result = box->addCard(card);
+  if (result == nullptr) {
+    removeCard(card);
+    return true;
+  }
+  return false; // Failed to add to the box
+}
+
+Card *User::retrieveCard(Box *box, const std::string &cardId) {
+  if (!box) {
+    return nullptr; // Invalid box
+  }
+  Card *card = box->retrieveCard(username, cardId, password);
+  assert(cards.find(card) == cards.end() &&
+         "Card should not be in user's collection after retrieval");
+  if (card) {
+    addCard(card); // Add the card back to the user's collection
+  }
+  return card; // Return the retrieved card or nullptr if not found
+}
+
+void User::readMail(int index) const {
+  if (emailServer) {
+    const Email *email =
+        emailServer->getEmailById(username, emailPassword, index);
+    cout << "Reading email #" << index << ":" << "\n";
+    cout << "Subject: " << email->subject << "\n";
+    cout << "Body: " << email->body << "\n";
+    cout << "From: " << email->sender << endl;
+  } else {
+    std::cerr << "Email server not set for user: " << username << std::endl;
+  }
+}
+
+set<long long> User::getEmailIds() const {
+  if (emailServer) {
+    return emailServer->getEmails(username, emailPassword);
+  }
+  return {}; // Return an empty set if email server is not set
+}
--- src/User.h
+++ src/User.h
@@ -5,6 +5,8 @@
 #include <string>
 
 class Card;
+class Box;
+class EmailServer;
 
 class User {
 private:
@@ -12,13 +14,51 @@ private:
   std::string emailPassword;
   std::string password; // Password for the user
   std::string email;
-  std::set<Card *> cards; // Cards owned by the user
+  std::set<Card *> cards;   // Cards owned by the user
+  EmailServer *emailServer; // Pointer to the email server for communication
 
 protected:
 public:
   User(const std::string &usrName, const std::string &passwd,
-       const std::string &emailAddr, const std::string &emailPasswd);
+       EmailServer *emailServer, const std::string &emailAddr,
+       const std::string &emailPasswd);
   virtual ~User();
+
+  /**
+   * @brief add a card to the user's collection
+   * @param card: pointer to the card to be added
+   */
+  void addCard(Card *card);
+  /**
+   * @brief remove a card from the user's collection
+   * @param card: pointer to the card to be removed
+   */
+  void removeCard(Card *card);
+  /**
+   * @brief drop a card into a box
+   * @param box: pointer to the box where the card will be dropped
+   * @param card: pointer to the card to be dropped
+   * @retval true: the card is dropped successfully
+   *         false: otherwise
+   */
+  bool dropCard(Box *box, Card *card);
+  /**
+   * @brief retrieve a card from a box
+   * @param box: pointer to the box where the card will be retrieved from
+   * @param cardId: the id of the card to be retrieved
+   * @return pointer to the retrieved card if successful,
+   *         otherwise nullptr
+   */
+  Card *retrieveCard(Box *box, const std::string &cardId);
+  /**
+   * @brief read the mail by index
+   * @param index: the index of the mail to be read
+   */
+  void readMail(int index) const;
+  /**
+   * @brief get all email IDs associated with the user
+   */
+  std::set<long long> getEmailIds() const;
 };
 
 #endif // USER_H
